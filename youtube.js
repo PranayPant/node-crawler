6c9f9ac9f0510b0c287c45f4cd31dd706d6547dd
PranayPant <ppantg1@gmail.com>
2017-03-29 22:28:37 -0500

    search url added

--- youtube.js
+++ youtube.js
@@ -2,23 +2,29 @@ const youtube 		= require('youtube-dl');
 const fs 			= require('fs');
 const DEBUG 		= require('./config.js').getDEBUG();
 
+const WHITESPACE 	= " ";
+const DELIMITER 	= "+";
+const BASE_URL 		= "https://www.youtube.com/";
+const SEARCH_SUFFIX = "results?search_query=";
+
+// Exports
+
 function download(url, file) {
 	var video = youtube(url);
-
 	video
-		.on('info', function(info) {
-			if(DEBUG) {
-				console.log('Download started');
-				console.log('filename: ' + info._filename);
-				console.log('size: ' + info.size);
-			}
-		})
+		.on('info', log)
 		.pipe(fs.createWriteStream(file))
-		.on('close', () => {
-			if (DEBUG) {
-				console.log("Finished downloading file!");
-			}
-		});
+		.on('close', completedDownload);
+}
+
+// Helpers
+
+function getVideo(searchUrl) {
+	
+}
+
+function generateUrl(query) {
+	return BASE_URL + SEARCH_SUFFIX + query.split(WHITESPACE).join(DELIMITER);
 }
 
 function log(info) {
