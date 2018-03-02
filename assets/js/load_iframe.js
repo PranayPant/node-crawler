64aa86b6e1d6d9a38d76b03339ea5e33808b9611
PranayPant <ppantg1@gmail.com>
2018-03-01 21:12:38 -0600

    looking nice :)

--- assets/js/load_iframe.js
+++ assets/js/load_iframe.js
@@ -4,22 +4,13 @@ window.load_iframe = function(elem) {
 	var src = $(elem).attr('embed-url');
 
 	var iframe = document.createElement("iframe");
-	iframe.setAttribute( "width", "246" );
 	iframe.setAttribute( "frameborder", "0" );
 	iframe.setAttribute( "allowfullscreen", "" );
 	iframe.setAttribute( "src", src );
+	iframe.setAttribute( "class", "thumbnail" );
 
 	$(elem).html("");
 	$(elem).html($(iframe).prop('outerHTML'));
 }
 
-function sleep(milliseconds) {
-	var start = new Date().getTime();
-	for (var i = 0; i < 1e7; i++) {
-		if ((new Date().getTime() - start) > milliseconds){
-			break;
-		}
-	}
-}
-
-module.exports 	= load_iframe;
+module.exports = load_iframe;
